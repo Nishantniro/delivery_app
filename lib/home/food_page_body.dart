d2f6e2631d725c10190a3955399c51c616bda06a
Nishant Niroula <niroulanishant910@gmail.com>
2025-04-21 12:08:58 +0545

    apply stack container

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -8,12 +8,15 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pagecontroller = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.red,
       height: 320,
       child: PageView.builder(
         itemCount: 5,
+        controller: pagecontroller,
         itemBuilder: (context, position) {
           return _buildPageItem(position);
         },
@@ -22,20 +25,35 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 20, right: 20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color:
-            index.isEven
-                ? Color(0xFF69c5df)
-                : Color.fromARGB(255, 204, 190, 146),
-        image: DecorationImage(
-          image: AssetImage("assets/images/food0.png"),
-          fit: BoxFit.cover,
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color:
+                index.isEven
+                    ? Color(0xFF69c5df)
+                    : Color.fromARGB(255, 204, 190, 146),
+            image: DecorationImage(
+              image: AssetImage("assets/images/food0.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 130,
+            margin: EdgeInsets.only(left: 38, right: 38, bottom: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
