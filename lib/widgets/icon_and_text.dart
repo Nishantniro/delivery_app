8dabfc2b678281d8a9cd4b840f2764ec63dc3ecb
Nishant Niroula <niroulanishant910@gmail.com>
2025-04-26 18:01:11 +0545

    small changes

--- lib/widgets/icon_and_text.dart
+++ lib/widgets/icon_and_text.dart
@@ -4,14 +4,12 @@ import 'package:khamkham/widgets/small_text.dart';
 class IconAndText extends StatelessWidget {
   final String text;
   final IconData icon;
-  final Color color;
   final Color iconColor;
   const IconAndText({
     super.key,
     required this.text,
     required this.icon,
     required this.iconColor,
-    required this.color,
   });
 
   @override
@@ -20,7 +18,7 @@ class IconAndText extends StatelessWidget {
       children: [
         Icon(icon, color: iconColor),
         SizedBox(width: 5),
-        SmallText(text: text, color: color),
+        SmallText(text: text),
       ],
     );
   }
