4d756c0f57bb1e2eed6672ce5f9942d11efb5aa3
Nishant Niroula <niroulanishant910@gmail.com>
2025-04-29 08:40:03 +0545

    added scaling eqn in scrollable list

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -14,6 +14,7 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pagecontroller = PageController(viewportFraction: 0.85);
   var _CurrPageValue = 0.0;
+  double _ScaleFactor = 0.8;
 
   @override
   void initState() {
@@ -47,87 +48,97 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 9, right: 9),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color:
-                index.isEven
-                    ? Color(0xFF69c5df)
-                    : Color.fromARGB(255, 204, 190, 146),
-            image: DecorationImage(
-              image: AssetImage("assets/images/food0.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 130,
-            width: 400,
-            margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
+    Matrix4 matrix4 = new Matrix4.identity();
+    if (index == _CurrPageValue.floor()) {
+      var currScale = 1 - (_CurrPageValue - index) * (1 - _ScaleFactor);
+    } else if (index == _CurrPageValue.floor() + 1) {
+      var currScale =
+          _ScaleFactor + (_CurrPageValue - index + 1) * (1 - _ScaleFactor);
+    }
+    return Transform(
+      transform: matrix4,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 9, right: 9),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-            ),
-            child: Container(
-              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Chowmein", color: Colors.black54),
-                  SizedBox(height: 5),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(5, (index) {
-                          return Icon(
-                            Icons.star,
-                            color: AppColors.mainColor,
-                            size: 17,
-                          );
-                        }),
-                      ),
-                      SizedBox(width: 5),
-                      SmallText(text: "4.5"),
-                      SizedBox(width: 10),
-                      SmallText(text: "1287"),
-                      SizedBox(width: 5),
-                      SmallText(text: "comments"),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  Row(
-                    children: [
-                      IconAndText(
-                        icon: Icons.circle_sharp,
-                        iconColor: AppColors.iconColor1,
-                        text: 'Normal',
-                      ),
-                      SizedBox(width: 10),
-                      IconAndText(
-                        icon: Icons.location_on,
-                        iconColor: AppColors.mainColor,
-                        text: '1.7km',
-                      ),
-                      SizedBox(width: 10),
-                      IconAndText(
-                        icon: Icons.access_time_rounded,
-                        iconColor: AppColors.iconColor2,
-                        text: '20min',
-                      ),
-                    ],
-                  ),
-                ],
+              color:
+                  index.isEven
+                      ? Color(0xFF69c5df)
+                      : Color.fromARGB(255, 204, 190, 146),
+              image: DecorationImage(
+                image: AssetImage("assets/images/food0.png"),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-        ),
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 130,
+              width: 400,
+              margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chowmein", color: Colors.black54),
+                    SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              color: AppColors.mainColor,
+                              size: 17,
+                            );
+                          }),
+                        ),
+                        SizedBox(width: 5),
+                        SmallText(text: "4.5"),
+                        SizedBox(width: 10),
+                        SmallText(text: "1287"),
+                        SizedBox(width: 5),
+                        SmallText(text: "comments"),
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    Row(
+                      children: [
+                        IconAndText(
+                          icon: Icons.circle_sharp,
+                          iconColor: AppColors.iconColor1,
+                          text: 'Normal',
+                        ),
+                        SizedBox(width: 10),
+                        IconAndText(
+                          icon: Icons.location_on,
+                          iconColor: AppColors.mainColor,
+                          text: '1.7km',
+                        ),
+                        SizedBox(width: 10),
+                        IconAndText(
+                          icon: Icons.access_time_rounded,
+                          iconColor: AppColors.iconColor2,
+                          text: '20min',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
