28813a03c3a5391d35571556896ff024478aeb6e
Nishant Niroula <niroulanishant910@gmail.com>
2025-05-06 09:00:55 +0545

    added container height dynamically slider container

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 class Dimensions {
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight / 2.56;
   static double pageViewContainer = screenHeight / 3.84;
   static double pageViewTextContainer = screenHeight / 6;
 }
