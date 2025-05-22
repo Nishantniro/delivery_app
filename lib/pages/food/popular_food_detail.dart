18d1c837aedd6bf274567631aac8874f5cf02ce1
Nishant Niroula <niroulanishant910@gmail.com>
2025-05-22 22:55:10 +0545

    added reusable widget column

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:khamkham/utils/colors.dart';
 import 'package:khamkham/utils/dimensions.dart';
+import 'package:khamkham/widgets/app_column.dart';
 import 'package:khamkham/widgets/app_icon.dart';
 import 'package:khamkham/widgets/big_text.dart';
 import 'package:khamkham/widgets/icon_and_text.dart';
@@ -62,55 +63,7 @@ class PopularFoodDetail extends StatelessWidget {
                   topRight: Radius.circular(Dimensions.radius20),
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Chowmein", color: Colors.black),
-                  SizedBox(height: Dimensions.height5),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(5, (index) {
-                          return Icon(
-                            Icons.star,
-                            color: AppColors.mainColor,
-                            size: 17,
-                          );
-                        }),
-                      ),
-                      SizedBox(width: 5),
-                      SmallText(text: "4.5"),
-                      SizedBox(width: 10),
-                      SmallText(text: "1287"),
-                      SizedBox(width: 5),
-                      SmallText(text: "comments"),
-                    ],
-                  ),
-                  SizedBox(height: Dimensions.height20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconAndText(
-                        icon: Icons.circle_sharp,
-                        iconColor: AppColors.iconColor1,
-                        text: 'Normal',
-                      ),
-                      SizedBox(width: 10),
-                      IconAndText(
-                        icon: Icons.location_on,
-                        iconColor: AppColors.mainColor,
-                        text: '1.7km',
-                      ),
-                      SizedBox(width: 10),
-                      IconAndText(
-                        icon: Icons.access_time_rounded,
-                        iconColor: AppColors.iconColor2,
-                        text: '20min',
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+              child: AppColumn(text: "Chicken Chowmein"),
             ),
           ),
         ],
