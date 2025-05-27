5a8250f4470a0318e080ba878007df5bdeb7c54b
Nishant Niroula <niroulanishant910@gmail.com>
2025-05-27 22:03:52 +0545

    added the logic for the show less and show more and hidden text in dart

---
+++ lib/widgets/expendable_text_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:khamkham/utils/dimensions.dart';
+
+class ExpendableTextWidget extends StatefulWidget {
+  final String text;
+  const ExpendableTextWidget({super.key, required this.text});
+
+  @override
+  State<ExpendableTextWidget> createState() => _ExpendableTextWidgetState();
+}
+
+class _ExpendableTextWidgetState extends State<ExpendableTextWidget> {
+  late String firstHalf;
+  late String secondHalf;
+  bool hiddenText = true;
+  double textHeight = Dimensions.screenHeight / 6.5;
+
+  @override
+  void initState() {
+    // Initialize firstHalf and secondHalf based on the text length
+    // If the text is longer than textHeight, split it into two halves
+    // Otherwise, keep firstHalf as the full text and secondHalf as an empty string
+    super.initState();
+    if (widget.text.length > textHeight) {
+      firstHalf = widget.text.substring(0, textHeight.toInt());
+      secondHalf = widget.text.substring(
+        textHeight.toInt() + 1,
+        widget.text.length,
+      );
+    } else {
+      firstHalf = widget.text;
+      secondHalf = "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
