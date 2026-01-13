1aa9d59cdbbd9acd0054d951de94a28af10071c1
Nishant Niroula <niroulanishant910@gmail.com>
2026-01-13 22:29:14 +0545

    + controller -> Popular_product_list

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,18 +1,19 @@
-// import 'package:get/get.dart';
-// import 'package:khamkham/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+import 'package:khamkham/data/repository/popular_product_repo.dart';
 
-// class PopularProductController extends GetxController {
-//   final PopularProductRepo popularProductRepo;
-//   PopularProductController({required this.popularProductRepo});
-//   List<dynamic> _popularProductList = [];
-//   List<dynamic> get popularProductList => _popularProductList;
+class PopularProductController extends GetxController {
+  final PopularProductRepo popularProductRepo;
+  PopularProductController({required this.popularProductRepo});
 
-//   Future<void> getPopularProductList() async {
-//     Response response = await popularProductRepo.getPopularProductList();
-//     if (response.statusCode == 200) {
-//       _popularProductList = [];
-//       //_popularProductList.addAll();
-//       update();
-//     } else {}
-//   }
-// }
+  List<dynamic> _popularProductList = [];
+  List<dynamic> get popularProductList =>
+      _popularProductList; // getting data from private variable
+  Future<void> getPopularProductList() async {
+    Response response = await popularProductRepo.getPopularProductList();
+    if (response.statusCode == 200) {
+      _popularProductList = [];
+      _popularProductList.addAll();
+      update();
+    } else {}
+  }
+}
