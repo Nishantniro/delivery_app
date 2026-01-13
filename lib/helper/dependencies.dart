d5e92fe48a85ea449f19ebcbd499953b2dd0c917
Nishant Niroula <niroulanishant910@gmail.com>
2026-01-13 22:44:30 +0545

    + init() -> apiclient,repo,controller

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -14,8 +14,16 @@
 //   Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 // }
 import 'package:get/get.dart';
+import 'package:khamkham/controllers/popular_product_controller.dart';
 import 'package:khamkham/data/api/api_client.dart';
+import 'package:khamkham/data/repository/popular_product_repo.dart';
 
 Future<void> init() async {
+  //for api client
   Get.lazyPut(() => ApiClient(appBaseUrl: "www.example.com"));
+  // for repos
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+
+  // for controllers
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 }
